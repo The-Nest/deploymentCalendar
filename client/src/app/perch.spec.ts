78e1dd2dbe27e23d1d6e1acf4fc28ffbb58302f3
morganTaylor193 <mtlee3@gmail.com>
2018-05-18 15:12:55 -0700

    feat: Added calendar card ocmponent and base styles for it.

--- client/src/app/perch.spec.ts
+++ client/src/app/perch.spec.ts
@@ -1,25 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { Perch } from './Perch';
+import { PerchCalendarCard } from '../components/perch-calendar-card/perch-calendar-card';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        Perch, PerchCalendarCard
       ],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(Perch);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
   it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(Perch);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(Perch);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to client!');
---
+++ client/src/app/perch.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'perch',
+  templateUrl: './perch.html',
+  styleUrls: ['./perch.css']
+})
+
+export class Perch {
+  title = 'The Perch';
+}
