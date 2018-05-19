094798b7227236f5d5a2912e28762f301cc4479d
morganTaylor193 <mtlee3@gmail.com>
2018-05-19 07:51:02 -0700

    wip: Added normalise css, header, and basic styles.

---
+++ client/src/components/perch-header/perch-header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'perch-header',
+  templateUrl: './perch-header.html',
+  styleUrls: ['./perch-header.scss']
+})
+
+export class PerchHeader {}
