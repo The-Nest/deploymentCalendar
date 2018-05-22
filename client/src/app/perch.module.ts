2286a07203b497b33f39dc7fbae58574a6151ec1
morganTaylor193 <mtlee3@gmail.com>
2018-05-21 20:37:27 -0700

    feat: Added in base routes and router configuration.

--- client/src/app/perch.module.ts
+++ client/src/app/perch.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Perch } from './perch';
-import { PerchCalendarCard } from '../components/perch-calendar-card/perch-calendar-card';
+import { RouterModule, Routes } from '@angular/router';
 import { PerchHeader } from '../components/perch-header/perch-header';
-
+import { PerchHomePage } from '../containers/homePage/perch-home-page';
+import { PerchAllDeploymentsPage } from '../containers/allDeploymentsPage/perch-all-deployments-page';
+import { Routing } from '../../routes/routes';
 
 @NgModule({
   declarations: [
     Perch,
-    PerchCalendarCard,
-    PerchHeader
+    PerchHeader,
+    PerchHomePage,
+    PerchAllDeploymentsPage
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Routing
   ],
   providers: [],
   bootstrap: [Perch]
---
+++ client/src/containers/homePage/perch-home-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'perch-home-page',
+  templateUrl: './perch-home-page.html',
+  styleUrls: ['./perch-home-page.scss']
+})
+
+export class PerchHomePage {}
